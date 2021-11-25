adb1ed08048a09859b2b8bd70e291b22ebeb6fc1
Mark <md.gortemaker@gmail.com>
2021-11-25 16:02:45 +0100

    Login met string wachwoord

--- MovieMatcher/Login.xaml.cs
+++ MovieMatcher/Login.xaml.cs
@@ -24,17 +24,44 @@ namespace MovieMatcher
             InitializeComponent();
         }
 
+        private void UsernameChangedEventHandler(object sender, TextChangedEventArgs args)
+        {
+            if (borderUsername.BorderBrush == Brushes.Red)
+            {
+                borderUsername.BorderBrush = Brushes.Black;
+            }
+        }
+        private void PasswordChangedEventHandler(object sender, RoutedEventArgs e)
+        {
+            if (borderPassword.BorderBrush == Brushes.Red)
+            {
+                borderPassword.BorderBrush = Brushes.Black;
+            }
+        }
+
         private void Button_Login_Click(object sender, RoutedEventArgs e)
         {
-            Database D = new Database();
-            var result = D.CheckPassword(txtUsername.Text.ToString(), txtPassword.Password.ToString());
-            Label_Result.Content = result;
-            if (String.IsNullOrEmpty(result))
+            if (String.IsNullOrEmpty(txtUsername.Text))
             {
-                MessageBox.Show($"FOUT1 {result}");
+                borderUsername.BorderBrush = Brushes.Red;
             }
-            
-            
+            if (String.IsNullOrEmpty(txtPassword.Password))
+            {
+                borderPassword.BorderBrush = Brushes.Red;
+                
+            } else
+            {
+                Database D = new Database();
+                
+                if (D.CheckPassword(txtUsername.Text.ToString(), txtPassword.Password.ToString()))
+                {
+                    //naar home scherm
+                    MessageBox.Show($"Inloggegevens juist ;)");
+                }
+                else
+                    MessageBox.Show($"Inloggegevens onjuist");
+            }
+
         }
 
         private void Button_Register_Click(object sender, RoutedEventArgs e)
---
+++ MovieMatcher/Models/Database/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualBasic;
+
+namespace MovieMatcher.Models.Database
+{
+    class UserInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string birth_year { get; set; }
+    }
+}
