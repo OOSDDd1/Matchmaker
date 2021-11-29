67583c0a9520f2594ab8058154f4660ef38094e2
Jesse <s1161980@student.windesheim.nl>
2021-11-29 14:14:27 +0100

    missed something

--- MovieMatcher/Models/PlaceHolderAccount.cs
+++ MovieMatcher/Models/PlaceHolderAccount.cs
@@ -8,5 +8,15 @@ namespace MovieMatcher.Models
 {
     class PlaceHolderAccount
     {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public PlaceHolderAccount(string name, string email, DateTime birthdate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthdate;
+        }
     }
 }
