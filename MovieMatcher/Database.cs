f6b46876e8615bc87d6342148d52ddbb88e892c2
Daan <daanrosendal@outlook.com>
2021-11-29 13:49:05 +0100

    remove unused directives

--- MovieMatcher/Database.cs
+++ MovieMatcher/Database.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Data.Common;
-using System.Diagnostics;
-using Microsoft.Data.SqlClient;
-using System.Text;
+﻿using Microsoft.Data.SqlClient;
 
 namespace MovieMatcher
 {
