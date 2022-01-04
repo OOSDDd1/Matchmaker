c3190a2160dd7f429f0802b1b0746b5cb5cc1e58
Dion Potkamp <dion.potkamp@gmail.com>
2022-01-04 16:49:26 +0100

    Add DetailView trailer playing workaround

--- MovieMatcher/Views/HistoryView.xaml.cs
+++ MovieMatcher/Views/HistoryView.xaml.cs
@@ -153,7 +153,7 @@ namespace MovieMatcher.Views
             DetailViewStore.Id = tmp.id;
             DetailViewStore.MediaType = tmp.mediaType;
 
-            Application.Current.Windows[0].DataContext = new DetailView();
+            Application.Current.Windows[0].DataContext = MainWindow.DetailView_Clicked();
         }
     }
 }
