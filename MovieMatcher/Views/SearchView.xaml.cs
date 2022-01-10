64001b21872374e3c496c3a0fffe1e937389efa7
Jesse <s1161980@student.windesheim.nl>
2022-01-10 14:05:43 +0100

    Changed Api to allow Adult to have it's own value

--- MovieMatcher/Views/SearchView.xaml.cs
+++ MovieMatcher/Views/SearchView.xaml.cs
@@ -25,7 +25,7 @@ namespace MovieMatcher.Views
         //when the searchbutton is clicked, this function will fire filling the listbox with items
         private void SearchButton_Clicked(object sender, RoutedEventArgs e)
         {
-            if (!ApiService.Search(searchTxt.Text, out var getMovieResult, (GreaterThan18(UserStore.birthYear ?? DateTime.Now) & UserStore.adult == true)))
+            if (!ApiService.Search(searchTxt.Text, out var getMovieResult, (bool)UserStore.adult))
                 return;
 
             Grid.SetRow(SearchBar, 0);
